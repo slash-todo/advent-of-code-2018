2dd11627c6157f326ddf6f1cda22ebc2157e79b6
Scott Robertson <robertsons@marketingresults.net>
2018-12-05 13:10:51 -0500

    solved day 2, puzzle b

--- 02/skat/02-b.js
+++ 02/skat/02-b.js
@@ -1,3 +1,40 @@
-const inputs = require('./input.js');
+console.clear();
+const idList = require('./input.js');
 
-// soon
+// Check Each ID
+for (let i = 0; i < idList.length; i++) {
+    // Check Each ID against Each Ahead of it
+    for (let j = i + 1; j < idList.length; j++) {
+        // break the ids in arrays
+        let a = idList[i].split('');
+        let b = idList[j].split('');
+
+        for (let k = 0; k < a.length - 1; k++) {
+            // copy a & b
+            let aa = a.slice(0);
+            let bb = b.slice(0);
+
+            // remove k from a and b
+            aa.splice(k, 1);
+            bb.splice(k, 1);
+
+            // assume they match until dissproven
+            let matchFlag = true;
+
+            // check if a & b match without k
+            for (let l = 0; l < aa.length; l++) {
+                if (matchFlag === true) {
+                    if (aa[l] !== bb[l]) {
+                        matchFlag = false;
+                    }
+                } else {
+                    break;
+                }
+            }
+            if (matchFlag) {
+                console.log(i, j, k);
+                console.log(aa.join(''));
+            }
+        }
+    }
+}
