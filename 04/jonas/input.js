d2f3051098cb59d20b576ba8302441e017cfff2c
Jonas Gierer <jgierer12@gmail.com>
2018-12-05 01:45:42 +0100

    Add day 04 input

---
+++ 04/jonas/input.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(`${__dirname}/input.txt`)
+  .toString()
+  .trim()
+  .split("\n");
+
+const regex = /\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\] ((Guard #(\d+) (begins shift))|(falls asleep|wakes up))/;
+
+let guardId;
+const result = input.sort().map(line => {
+  console.log(line);
+  let [
+    ,
+    year,
+    month,
+    day,
+    hour,
+    minute,
+    ,
+    ,
+    newGuardId,
+    activity1,
+    activity2
+  ] = regex.exec(line);
+  if (newGuardId) guardId = newGuardId;
+  return {
+    year: Number(year),
+    month: Number(month),
+    day: Number(day),
+    hour: Number(hour),
+    minute: Number(minute),
+    guardId,
+    activity: activity1 || activity2
+  };
+});
+
+module.exports = result;
