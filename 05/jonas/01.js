3157a57423e5b5a6e334b214ec9db33e2a71110c
Jonas Gierer <jgierer12@gmail.com>
2018-12-06 22:21:58 +0100

    Shorten 05/01 solution

--- 05/jonas/01.js
+++ 05/jonas/01.js
@@ -2,11 +2,7 @@ const input = require("./input");
 const { tail, areReactive } = require("./utils");
 
 const result = input.reduce((acc, char) => {
-  if (acc.length === 0 || !areReactive(tail(acc), char)) {
-    acc.push(char);
-  } else {
-    acc.pop();
-  }
+  tail(acc) && areReactive(tail(acc), char) ? acc.pop() : acc.push(char);
   return acc;
 }, []);
 
